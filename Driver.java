b8e9ccdddc2b5d35fce41724073cc8fa0d242c63
Ajay Narayanan Anantha Subramanian <ajay@Ajays-MacBook-Pro.local>
2024-09-29 20:37:50 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,119 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Driver {
-    public static void main(String [] args) {
+
+    public static void main(String[] args) throws IOException {
+        boolean allTestsPassed = true;
+
+        // Test default constructor
         Polynomial p = new Polynomial();
-        System.out.println(p.evaluate(3));
-        double [] c1 = {6,0,0,5};
-        Polynomial p1 = new Polynomial(c1);
-        double [] c2 = {0,-2,0,0,-9};
-        Polynomial p2 = new Polynomial(c2);
-        Polynomial s = p1.add(p2);
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1))
-            System.out.println("1 is a root of s");
-        else
-            System.out.println("1 is not a root of s");
+        if (p.evaluate(0) != 0) {
+            allTestsPassed = false;
+            System.out.println("Test case failed: Default constructor test");
+        }
+
+        // Test constructor with coefficients array
+        double[] coefficients = {6, 0, 0, 5};
+        Polynomial p1 = new Polynomial(coefficients);
+        if (Math.abs(p1.evaluate(3) - (6 + 5 * Math.pow(3, 3))) >= 1e-9) {
+            allTestsPassed = false;
+            System.out.println("Test case failed: Constructor with coefficients test");
+        }
+
+        // Test case: 5-3x2+x+7x8 (File constructor)
+        FileWriter writer1 = new FileWriter("test1.txt");
+        writer1.write("5-3x2+x+7x8");
+        writer1.close();
+        File file1 = new File("test1.txt");
+        Polynomial poly1 = new Polynomial(file1);
+        if (Math.abs(poly1.evaluate(1) - (5 - 3 * Math.pow(1, 2) + 1 * Math.pow(1, 1) + 7 * Math.pow(1, 8))) >= 1e-9) {
+            allTestsPassed = false;
+            System.out.println("Test case failed: 5-3x2+x+7x8 test");
+        }
+
+        // Test case: Add two polynomials: (6 + 5x^3) + (3 + 2x^2 + x^1)
+        double[] coefficients2 = {3, 1, 2};  // Corresponds to 3 + x + 2x^2
+        Polynomial p2 = new Polynomial(coefficients2);
+        Polynomial sum = p1.add(p2);  // Add p1 and p2
+
+        if (Math.abs(sum.evaluate(1) - (6 + 5 * Math.pow(1, 3) + 3 + 1 * Math.pow(1, 1) + 2 * Math.pow(1, 2))) >= 1e-9) {
+            allTestsPassed = false;
+            System.out.println("Test case failed: Add polynomials test");
+        }
+
+        // Test case: Multiply two polynomials: (6 + 5x^3) * (3 + 2x^2 + x^1)
+        Polynomial product = p1.multiply(p2);  // Multiply p1 and p2
+        if (Math.abs(product.evaluate(1) - ((6 + 5 * Math.pow(1, 3)) * (3 + 1 * Math.pow(1, 1) + 2 * Math.pow(1, 2)))) >= 1e-9) {
+            allTestsPassed = false;
+            System.out.println("Test case failed: Multiply polynomials test");
+        }
+
+        // Test case: -x2 + 4x - 7 (File constructor)
+        FileWriter writer2 = new FileWriter("test2.txt");
+        writer2.write("-x2+4x-7");
+        writer2.close();
+        File file2 = new File("test2.txt");
+        Polynomial poly2 = new Polynomial(file2);
+
+        if (Math.abs(poly2.evaluate(2) - (-Math.pow(2, 2) + 4 * 2 - 7)) >= 1e-9) {
+            System.out.println(poly2);
+            allTestsPassed = false;
+            System.out.println("Test case failed: -x2+4x-7 test");
+        }
+
+        // Test case: hasRoot method, check if 1 is a root of x^3 - x^2 + x - 1 (File constructor)
+        FileWriter writer3 = new FileWriter("test3.txt");
+        writer3.write("x3-x2+x-1");
+        writer3.close();
+        File file3 = new File("test3.txt");
+        Polynomial poly3 = new Polynomial(file3);
+        if (!poly3.hasRoot(1)) {
+            allTestsPassed = false;
+            System.out.println("Test case failed: hasRoot method test (should find 1 as root)");
+        }
+
+        // Test case: x^3 - x^2 + x - 1 (File constructor) for root failure
+        if (poly3.hasRoot(2)) {
+            allTestsPassed = false;
+            System.out.println("Test case failed: hasRoot method test (should not find 2 as root)");
+        }
+
+        // Test case: Saving a polynomial to a file
+        String filename = "saved_polynomial.txt";
+        poly1.saveToFile(filename);
+
+        // Reading the saved file and checking contents
+        File savedFile = new File(filename);
+        Polynomial savedPoly = new Polynomial(savedFile);
+
+        if (Math.abs(savedPoly.evaluate(1) - poly1.evaluate(1)) >= 1e-9) {
+            allTestsPassed = false;
+            System.out.println("savedPoly: " + savedPoly);
+            System.out.println("sentPoly: " + poly1);
+            System.out.println("Test case failed: Save and read polynomial test");
+        }
+
+        if (allTestsPassed) {
+            System.out.println("All test cases passed!");
+        } else {
+            System.out.println("Some test cases failed.");
+        }
+        FileWriter writerr = new FileWriter("ppoly1.txt");
+        writerr.write("0.4x2+0.02x+0.1");
+        writerr.close();
+        FileWriter writerr2 = new FileWriter("ppoly2.txt");
+        writerr2.write("-0.1");
+        writerr2.close();
+        Polynomial ppoly1 = new Polynomial(new File("ppoly1.txt"));
+        Polynomial ppoly2 = new Polynomial(new File("ppoly2.txt"));
+        System.out.println(ppoly2);
+        Polynomial resultA = (ppoly1.multiply(ppoly2));
+        System.out.println(resultA);
+        resultA.saveToFile("resultA.txt");
+        Polynomial resultB = ppoly2.add(ppoly1);
+        System.out.println(resultB);
+        resultB.saveToFile("resultB.txt");
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,49 +1,217 @@
+import java.util.*;
+import java.io.*;
 public class Polynomial{
-    double[] coefficients;
+    double[] non_zero_coefficients;
+    int[] exponents;
+    HashMap<Integer,Double> expToCE;
     public Polynomial(){
-        coefficients = new double[1];
-        coefficients[0] = 0.0;
+        non_zero_coefficients = new double[0];
+        exponents = new int[0];
+        expToCE = new HashMap<Integer,Double>();
     }
     public Polynomial(double[] coefficients){
-        this.coefficients = new double[coefficients.length];
+        int non_zero_count = 0;
         for(int i = 0; i < coefficients.length; i++){
-            this.coefficients[i] = coefficients[i];
+            if(coefficients[i] != 0){
+                non_zero_count++;
+            }
+        }
+        non_zero_coefficients = new double[non_zero_count];
+        exponents = new int[non_zero_count];
+        expToCE = new HashMap<Integer,Double>();
+        int index  = 0;
+        for(int i = 0; i < coefficients.length; i++){
+            if(coefficients[i] == 0){
+                continue;
+            }
+            non_zero_coefficients[index] = coefficients[i];
+            exponents[index] = i;
+            expToCE.put(i,coefficients[i]);
+            index++;
+        }
+
+    }
+    public Polynomial(double[] non_zero_coefficientss, int[] exponentss){
+        expToCE = new HashMap<Integer,Double>();
+        this.non_zero_coefficients = new double[non_zero_coefficientss.length];
+        this.exponents = new int[non_zero_coefficientss.length];
+        for(int i = 0; i < non_zero_coefficientss.length; i++){
+            this.non_zero_coefficients[i] = non_zero_coefficientss[i];
+            this.exponents[i] = exponentss[i];
+        }
+
+        //Sanitation
+        for(int i = 0; i < exponents.length; i++){
+            if(expToCE.containsKey(exponents[i])){
+                expToCE.put(exponents[i],expToCE.get(exponents[i]) + non_zero_coefficients[i]);
+            }
+            else{
+                expToCE.put(exponents[i],non_zero_coefficients[i]);
+            }
+
+        }
+        int theSize = expToCE.keySet().size();
+        for(Integer k : expToCE.keySet()){
+            if(expToCE.get(k) == 0){
+                theSize--;
+            }
+        }
+        exponents = new int[theSize];
+        non_zero_coefficients = new double[theSize];
+        int j = 0;
+        for(Integer k : expToCE.keySet()){
+            if(expToCE.get(k) == 0){
+                continue;
+            }
+            exponents[j] = k;
+            non_zero_coefficients[j] = expToCE.get(k);
+            j++;
         }
     }
     public Polynomial add(Polynomial that){
-        int len1 = this.coefficients.length;
-        int len2 = that.coefficients.length;
+        int len1 = this.non_zero_coefficients.length;
+        int len2 = that.non_zero_coefficients.length;
         int min_len = len1 < len2 ? len1: len2;
-        boolean this_min_len = min_len == len1;
-        double[] ces = new double[len1 > len2 ? len1: len2];
-        for(int i = 0; i < min_len; i++){
-            ces[i] = this.coefficients[i]+that.coefficients[i];
+        HashSet<Integer> exponents = new HashSet<Integer>();
+        for(int i = 0; i < len1; i++){
+            exponents.add(this.exponents[i]);
         }
-        if(this_min_len){
-            for(int i = min_len; i < ces.length; i++){
-                ces[i] = that.coefficients[i];
-            }
+        for(int i = 0; i < len2; i++){
+            exponents.add(that.exponents[i]);
         }
-        else{
-            for(int i = min_len; i < ces.length; i++){
-                ces[i] = this.coefficients[i];
-            }
+        int[] eponents = new int[exponents.size()];
+        double[] goefficients = new double[exponents.size()];
+        int j = 0;
+        for(Integer i : exponents){
+            eponents[j] = i;
+            double coHere = (this.expToCE.containsKey(i) ? this.expToCE.get(i) : 0);
+            double coThere = (that.expToCE.containsKey(i) ? that.expToCE.get(i) : 0);
+            goefficients[j] = coHere + coThere;
+            j++;
         }
-        Polynomial result = new Polynomial(ces);
-        return result;
+        return new Polynomial(goefficients, eponents);
     }
     public double evaluate(double x){
         double sum = 0;
-        for(int i = 0; i < coefficients.length; i++){
+        for(int i = 0; i < non_zero_coefficients.length; i++){
             double product = 1;
-            for(int j = 0; j < i; j++){
+            for(int j = 0; j < exponents[i]; j++){
                 product *= x;
             }
-            sum += coefficients[i]*product;
+            sum += non_zero_coefficients[i]*product;
         }
         return sum;
     }
     public boolean hasRoot(double x){
         return evaluate(x) == 0.0;
+    }
+    public Polynomial multiply (Polynomial that){
+        Polynomial result = new Polynomial();
+        for(int i = 0; i < this.exponents.length; i++){
+            double[] ccoefficients = new double[that.exponents.length];
+            int[] eexponents = new int[that.exponents.length];
+            for(int j = 0; j < that.exponents.length; j++){
+                ccoefficients[j] = this.non_zero_coefficients[i]*that.non_zero_coefficients[j];
+                eexponents[j] = this.exponents[i]+that.exponents[j];
+            }
+            result = result.add(new Polynomial(ccoefficients,eexponents));
+        }
+        return result;
+
+    }
+    public Polynomial(File file) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String polynomialString = br.readLine();
+        br.close();
+        polynomialString = polynomialString.replace("-","+-");
+        polynomialString = polynomialString.replace("x+","x1+");
+        polynomialString = polynomialString.replace("+x","+1x");
+        polynomialString = polynomialString.replace("-x","-1x");
+        if(polynomialString.charAt(0) == 'x'){
+            polynomialString = "1"+polynomialString;
+        }
+        if(polynomialString.charAt(polynomialString.length()-1) == 'x'){
+            polynomialString += "1";
+        }
+        String[] terms = polynomialString.split("\\+");
+        exponents = new int[terms.length];
+        non_zero_coefficients = new double[terms.length];
+        expToCE = new HashMap<>();
+        for(int i = 0; i < terms.length; i++){
+            if(!terms[i].contains("x")){
+                if (terms[i].isEmpty()) {
+                    continue;
+                }
+                non_zero_coefficients[i] = Double.parseDouble(terms[i]);
+                exponents[i] = 0;
+            }
+            else{
+                String[] numbers = terms[i].split("x");
+                if (numbers[0].isEmpty()) {
+                    non_zero_coefficients[i] = 1.0;
+                }
+                else if (numbers[0].equals("-")) {
+                    non_zero_coefficients[i] = -1.0;
+                }
+                else {
+                    non_zero_coefficients[i] = Double.parseDouble(numbers[0]);
+                }
+                if (numbers.length > 1 && !numbers[1].isEmpty()) {
+                    exponents[i] = Integer.parseInt(numbers[1]);
+                }
+                else {
+                    exponents[i] = 1;
+                }
+            }
+        }
+
+        //Sanitation
+        for(int i = 0; i < exponents.length; i++){
+            if(expToCE.containsKey(exponents[i])){
+                expToCE.put(exponents[i],expToCE.get(exponents[i]) + non_zero_coefficients[i]);
+            }
+            else{
+                expToCE.put(exponents[i],non_zero_coefficients[i]);
+            }
+
+        }
+        int theSize = expToCE.keySet().size();
+        for(Integer k : expToCE.keySet()){
+            if(expToCE.get(k) == 0){
+                theSize--;
+            }
+        }
+        exponents = new int[theSize];
+        non_zero_coefficients = new double[theSize];
+        int j = 0;
+        for(Integer k : expToCE.keySet()){
+            if(expToCE.get(k) == 0){
+                continue;
+            }
+            exponents[j] = k;
+            non_zero_coefficients[j] = expToCE.get(k);
+            j++;
+        }
+    }
+    public void saveToFile(String filename) throws IOException {
+        String[] terms = new String[exponents.length];
+        for(int i = 0; i < exponents.length; i++){
+            terms[i] = non_zero_coefficients[i] + "x" + (exponents[i] == 1 ? "" : exponents[i]);
+        }
+        String res = String.join("+",terms);
+        res = res.replace("+-","-");
+        res = res.replace("+1x","+x");
+        res = res.replace("-1x","-x");
+        res = res.replace("x0","");
+        if(exponents.length == 0){
+            res = "0";
+        }
+        FileWriter writer = new FileWriter(filename);
+        writer.write(res);
+        writer.close();
+
+    }
+    public String toString(){
+        return "Exponents: " + Arrays.toString(exponents) + "\nCoefficients: " + Arrays.toString(non_zero_coefficients);
     }
 }
